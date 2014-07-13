ebacdc462b0745ab743fad4503fae6c547c1c08d
Zhang Hu <zhanghu@yunba.io>
2014-07-13 12:32:14 +0800

    ticket service

--- examples/test_basic.c
+++ examples/test_basic.c
@@ -10,6 +10,22 @@ testcb(evhtp_request_t * req, void * a) {
     const char * str = a;
 
     evbuffer_add_printf(req->buffer_out, "%s", str);
+    evhtp_headers_add_header(req->headers_out, evhtp_header_new("Content-Type", "application/json", 0, 0));
+    evhtp_send_reply(req, EVHTP_RES_OK);
+}
+
+int cur = 0;
+
+void
+rootcb(evhtp_request_t *req, void *a) {
+    const char *str[] = {
+	"{\"c\":\"tcp://119.9.78.46:1883\"}", 
+	"{\"c\":\"tcp://119.9.78.88:1883\"}"
+	};
+    cur++;
+    cur = cur % 2;
+    evbuffer_add_printf(req->buffer_out, "%s", str[cur]);
+    evhtp_headers_add_header(req->headers_out, evhtp_header_new("Content-Type", "application/json", 0, 0));
     evhtp_send_reply(req, EVHTP_RES_OK);
 }
 
@@ -21,6 +37,7 @@ main(int argc, char ** argv) {
     evhtp_set_cb(htp, "/simple/", testcb, "simple");
     evhtp_set_cb(htp, "/1/ping", testcb, "one");
     evhtp_set_cb(htp, "/1/ping.json", testcb, "two");
+    evhtp_set_cb(htp, "/", rootcb, "");
 #ifndef EVHTP_DISABLE_EVTHR
     evhtp_use_threads(htp, NULL, 4, NULL);
 #endif
